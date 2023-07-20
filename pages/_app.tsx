253cfb4e81a3d8972e7181cf80713dafad0b11b3
Miguel Camilo <miguelcamilo48@gmail.com>
2023-07-20 19:39:45 -0400

    CREATING modal component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Layout from '@/components/Layout';
+import Modal from '@/components/Modal';
 
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
@@ -6,7 +7,11 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
-      {/* <Modal /> */}
+			<Modal 
+				isOpen
+				actionLabel='Submit'
+				title='Welcome, Login!'
+			/>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
