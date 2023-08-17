ba30e91ef1779202a0ed26e23d2d0aad06045d5f
Miguel Camilo <miguelcamilo48@gmail.com>
2023-08-17 01:16:06 -0400

    ADDED a loading state if profile does not have post

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,24 +3,24 @@ import Followbar from './layout/Followbar';
 import MobileBar from './layout/Mobilebar';
 
 interface LayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <div className="max-h-screen bg-[#16181c]">
-      <div className="container h-full mx-auto max-w-[95rem]">
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 h-full">
-          <Sidebar />
-          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
-            {children}
-          </div>
-          <Followbar />
-        </div>
-        <MobileBar />
-      </div>
-    </div>
-  );
+	return (
+		<div className="max-h-screen bg-[#16181c]">
+			<div className="container mx-auto h-full max-w-[95rem]">
+				<div className="grid h-full sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+					<Sidebar />
+					<div className="col-span-3 border-x-[1px] border-neutral-800 lg:col-span-2">
+						{children}
+					</div>
+					<Followbar />
+				</div>
+				<MobileBar />
+			</div>
+		</div>
+	);
 };
 
 export default Layout;
--- components/posts/PostFeed.tsx
+++ components/posts/PostFeed.tsx
@@ -1,14 +1,45 @@
 import usePosts from '@/hooks/usePosts';
 import PostItem from './PostItem';
 
+import { ClipLoader } from 'react-spinners';
+
 interface PostFeedProps {
     userId?: string;
 }
 
 const PostFeed: React.FC <PostFeedProps> = ({
-    userId
+    userId,
 }) => {
-    const { data: posts = [] } = usePosts(userId) 
+    const { data: posts = [], isLoading } = usePosts(userId) 
+
+    
+	const randomQuotes = [
+		'This profile is currently as blank as my mind during an exam.',
+		'This page is giving me 404 error vibes with all its nothingness.',
+        "Empty...Dry...Nothing to see...just like my DM's",
+        "Is this the Krusty Krab? ...NO, this is Patrick!"
+	];
+
+    const randomQuotesIndex = Math.floor(Math.random() * randomQuotes.length)
+
+	if (isLoading || posts.length === 0) {
+		return (
+			<div
+				role="status"
+				className="space-y-8 md:flex md:items-center md:space-x-8 md:space-y-0"
+			>
+				<div className="flex flex-col h-[17rem] w-full items-center justify-center">
+					<p className="text-sm sm:text-md text-center font-bold text-neutral-400 italic">
+                        {`"${randomQuotes[randomQuotesIndex]}"`}
+					</p>
+                    <span className='text-xs italic text-center mt-1 text-neutral-500'>
+                        - DevLink
+                    </span>
+				</div>
+			</div>
+		);
+	}
+
     return ( 
         <>
             {posts.map((post: Record<string, any>) => (
